3e69823b3879fbfe0c28944f9af39ea4b32e9c61
Bunmi <bunmioye09@gmail.com>
2022-05-07 16:00:18 +0100

    add reusable components and global styles

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  primary: '#14F195',
+
+  accent: '#03E1FF',
+
+  // --gainsboro
+  // bg2: '#dddddd',
+
+  // --eerie-black
+  typeface: '#222222',
+
+  // --cultured
+  // bg: '#f0f0f0',
+};
+
+export default theme;
