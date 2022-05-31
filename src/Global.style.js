a0b861f007d2ba62c36f2150470adee8c2d5c6ff
Bunmi <bunmioye09@gmail.com>
2022-05-31 09:23:26 +0100

    remove primaryContainer

--- src/Global.style.js
+++ src/Global.style.js
@@ -1,18 +1,8 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
-
-export const PrimaryContainer = styled.div`
-  background-color: ${({ bgColor }) => (bgColor ? bgColor : '#fff')};
-  background-image: url(${({ bgImg }) => (bgImg ? bgImg : '')});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-
-  height: 100vh;
-`;
 
 const GlobalStyle = createGlobalStyle`
     * {
